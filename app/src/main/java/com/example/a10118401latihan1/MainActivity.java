de978379481eb8a87be8856f5ff4037eb19a8a76
daffasam <daffasacofi3@gmail.com>
2022-04-20 01:32:48 +0700

    Initial commit

---
+++ app/src/main/java/com/example/a10118401latihan1/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.a10118401latihan1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+// Tanggal Pengerjaan : 19 April 2022
+// NIM : 10118401
+// Nama : Daffa Sacofi Mutawakkil
+// Kelas : IF-6
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
